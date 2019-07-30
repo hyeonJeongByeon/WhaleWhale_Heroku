9f8c8e346b6d296517bce8c1b9f72859fdbd0824
hyeonJeongByeon <jungjung1218@gmail.com>
2019-07-30 19:33:58 -0400

    comment

--- whale/settings.py
+++ whale/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','gv-za@r#d+7-6_lsqu(t507f$x72nv-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool (os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['127.0.0.1', '10.10.99.145']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
