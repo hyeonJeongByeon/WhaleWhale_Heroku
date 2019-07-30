bbe0dce8ef23b5a287f0b1f471dcbbb9092dc592
hyeonJeongByeon <jungjung1218@gmail.com>
2019-07-30 19:07:31 -0400

    comment

--- whale/settings.py
+++ whale/settings.py
@@ -145,3 +145,7 @@ LOGIN_REDIRECT_URL = '/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'images')
 MEDIA_URL='/images/'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
